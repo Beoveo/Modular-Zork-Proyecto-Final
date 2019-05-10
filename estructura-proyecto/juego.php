761915c1f9958521aa8f51a1377120ba1d860562
lbarja <lbarja@ucm.es>
2019-05-10 21:09:29 +0200

    Delete ficheros viejos zork/javascript

--- estructura-proyecto/juego.php
+++ estructura-proyecto/juego.php
@@ -5,12 +5,13 @@ require_once __DIR__.'/includes/config.php';
 ?><!DOCTYPE html>
 <html>
     <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/estilo.css') ?>" />
-         <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/game.css') ?>" />
-     <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/estilo.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/game.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/estiloSidebarIz.css') ?>" />
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 
-      <title>Portada</title>
+        <title>Juego</title>
     </head>
     <body>
         <div id="contenedor">
@@ -20,6 +21,8 @@ require_once __DIR__.'/includes/config.php';
         $app->doInclude('contenidos/contenidoJuegoMod.php');
         $app->doInclude('comun/pie.php');
         ?>
+            <script type="text/javascript" src="zork/javascript/loadGame.js"></script>
+            <script type="text/javascript" src="zork/javascript/game.js"></script>
             <script type="text/javascript" src="zork/javascript/pruebaCuadricula.js"></script>
         </div>
     </body>
