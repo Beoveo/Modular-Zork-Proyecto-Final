c55c81ecb633915110ac06cd4aba4a53fe5777ab
lbarja <lbarja@ucm.es>
2019-05-27 10:29:01 +0200

    Responsive

--- estructura-proyecto/includes/comun/cabecera.php
+++ estructura-proyecto/includes/comun/cabecera.php
@@ -1,21 +1,12 @@
-
+<script type="text/javascript" src="css/menuResponsive.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <div id="cabecera">
+	<h1 id="titulo">
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a><!--Para abrir y cerrar el menu en ventanas pequeñas-->
+    Modular ZORK</h1>
 	
-    
-    <input type="checkbox" id="nav-check">
-    <h1>Modular ZORK </h1>
- 
-    <div class="nav-btn">
-        <label for="nav-check">
-            <span></span>
-            <span></span>
-            <span></span>
-    </label>
-    </div>
-  
-	
-    <div id="menu">
-	    <a href="index.php" class="button active" >Inicio</a>
+    <div class="topnav" id="menu">
+	    <a href="index.php" class="button" >Inicio</a>
 	    <a href="juego.php" class="button" >Juego</a>
 	    <a href="ranking.php" class="button" >Ranking</a>
 	    <a href="tienda.php" class="button" >Tienda</a>
@@ -35,5 +26,7 @@
                     <a href='logout.php' class='button' id='butRight'>Cerrar Sesión</a>";
                 }
             ?>
+        
+            
     </div>
 </div>
